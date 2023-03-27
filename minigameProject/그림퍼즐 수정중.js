b0241ead99df9485fba789da9b6e9e8e80233498
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-03-27 14:35:55 +0900

    일단저장

--- minigameProject/그림퍼즐 수정중.js
+++ minigameProject/그림퍼즐 수정중.js
@@ -6,13 +6,13 @@
     css 개선
  */
 
-    const originImage = ['minigameProject/image/0.png', 'minigameProject/image/1.png', 'minigameProject/image/2.png', 'minigameProject/image/3.png',
-    'minigameProject/image/4.png', 'minigameProject/image/5.png',
-    'minigameProject/image/6.png', 'minigameProject/image/7.png',
-    'minigameProject/image/8.png', 'minigameProject/image/9.png',
-    'minigameProject/image/10.png', 'minigameProject/image/11.png',
-    'minigameProject/image/12.png', 'minigameProject/image/13.png',
-    'minigameProject/image/14.png', null
+    const originImage = ['image/0.png', 'image/1.png', 'image/2.png', 'image/3.png',
+    'image/4.png', 'image/5.png',
+    'image/6.png', 'image/7.png',
+    'image/8.png', 'image/9.png',
+    'image/10.png', 'image/11.png',
+    'image/12.png', 'image/13.png',
+    'image/14.png', null
 ];
 
 function getImage() {
@@ -34,7 +34,7 @@ function getImage() {
         //     mixedImage.push(copyOfOrigin[mixedIndex]);
         //     copyOfOrigin[mixedIndex] = null;
         // }
-        // //==================테스트용===========================================
+        //==================테스트용===========================================
         const mixedIndex = orderedIndex;
         if (copyOfOrigin[mixedIndex] === null) {
             continue;
